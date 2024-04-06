6c99951a3b0b2cbe1c37c529c14c2be5d09d01bf
kwon <ksu619@naver.com>
2024-04-07 00:35:52 +0900

    cortex_m3_hello_world add

---
+++ cortex_m3_hello_world/src/main.rs
@@ -0,0 +1,60 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use stm32f1xx_hal::{
+    pac,
+    prelude::*,
+    serial::{Config, Serial},
+};
+use core::fmt::Write; // `write` 메서드를 사용하기 위해 필요합니다.
+use core::panic::PanicInfo;
+
+#[entry]
+fn main() -> ! {
+    let dp = pac::Peripherals::take().unwrap();
+    let _cp = cortex_m::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let rcc = dp.RCC.constrain();
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    //let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
+    let mut gpioa = dp.GPIOA.split();
+    let tx_pin = gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh);
+    let rx_pin = gpioa.pa10;
+
+    // afio의 MAPR를 사용하기 위한 초기화
+    //let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+    let mut afio = dp.AFIO.constrain();
+    //let serial = Serial::usart1(
+    //    dp.USART1,
+    //    (tx_pin, rx_pin),
+    //    &mut afio.mapr,
+    //    Config::default().baudrate(115200.bps()),
+    //    clocks,
+    //    &mut rcc.apb2,
+    //);
+    
+    let serial = Serial::usart1(
+        dp.USART1,
+        (tx_pin, rx_pin),
+        &mut afio.mapr,
+        Config::default().baudrate(115200.bps()),
+        clocks,
+    );
+    let (mut tx, _rx) = serial.split();
+
+    // writeln 대신 write를 사용
+    tx.write_str("Hello, world!\r\n").unwrap();
+
+    loop {
+        // 여기에 필요한 코드 추가
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
