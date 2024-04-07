82250f738c10bdf22ab8704b02acc6fe41118056
kwon <ksu619@naver.com>
2024-04-08 00:59:45 +0900

    inverse matrix add

--- matrix_cal/src/main.rs
+++ matrix_cal/src/main.rs
@@ -1,11 +1,14 @@
 extern crate ndarray;
+extern crate ndarray_linalg;
 
 use ndarray::Array2;
+use ndarray_linalg::Inverse;  
+
 
 fn main()
 {
     let k = 3.0;
-    let a = Array2::from_shape_vec((3, 3), vec![1.0, 2.0, k, 4.0, 1.0, 1.9, 4.0, 1.0, 1.9 ]).unwrap();
+    let a = Array2::from_shape_vec((3, 3), vec![1.0, 0.0, 0.0, 0.0, 1.0, 1.9, 0.0, k, 1.9 ]).unwrap();
     let b = Array2::from_shape_vec((3, 3), vec![5.0, 6.0, 7.0, 8.0, 1.0, 2.0, 8.0, 1.0, 2.0]).unwrap();
 
     // 행렬 곱셈
@@ -16,13 +19,8 @@ fn main()
     let result2 = &a + &b;
     println!("A + B:\n{}", result2);
 
-    loop {
-        // 서버에 메시지 전송
-        socket.send_to(result1, server_address)?;
-
-        println!("send message.");
-
-        // 1초 동안 대기
-        thread::sleep(Duration::from_secs(1));
+    match a.inv() {
+        Ok(inv_a) => println!("A의 역행렬:\n{}", inv_a),
+        Err(e) => println!("역행렬 계산 중 오류: {}", e),
     }
 }
